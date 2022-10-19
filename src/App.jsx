f32a9c349c8bae7387c58ac65c9681470b74fc6c
Ivan Montenegro <nelson.moncar@outlook.com>
2022-10-18 20:00:47 -0500

    creación de las primeras rutas, tocó react router 5 y no el 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import React from "react";
 import './styles/App.css'
 import { Titulo } from  './components/Titulo';
-import { CatalogoPelis } from './components/CatalogoPelis'
-
+import { CatalogoPelis } from './components/CatalogoPelis';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Titulo/>
-      <CatalogoPelis/>
-    </div>
+    <Router>
+      <header className="App">
+        <Titulo/>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/movie'}>Movie</Link>
+      </header>
+      <main>
+      </main>
+      <Switch>
+          <Route exact path="/movie">Movie</Route>
+          <Route path="/">Home</Route>
+      </Switch>
+    </Router>
   )
 }
 
