d7925fc34ccb838f8d0bfc3a7593544e71943361
Ivan Montenegro <nelson.moncar@outlook.com>
2022-10-21 17:30:07 -0500

    creación del form para busqueda y anexo de css al mismo

--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -1,8 +1,17 @@
-
+import '../styles/Searcher.css';
+import { ImSearch } from 'react-icons/im';
 
 function Searcher () {
     return (
-        <div> Buscador </div>
+        <form className={'searcherContainer'}>
+        <div className={'searchBox'}>
+            <input type="text" className={'searchInput'} />
+            <button type='submit' className={'searchButton'}>
+                <ImSearch className={'searchIcon'}/>
+            </button>
+        </div>
+        </form>
+        
     )
 };
 
