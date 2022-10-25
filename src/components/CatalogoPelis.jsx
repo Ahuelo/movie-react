4db8b2b0127c2d18a03b92cfb85db6e5c9aabb68
Ivan Montenegro <nelson.moncar@outlook.com>
2022-10-25 17:11:51 -0500

    creación del scroll infinito y arreglo del spinner mediante css

--- src/components/CatalogoPelis.jsx
+++ src/components/CatalogoPelis.jsx
@@ -3,38 +3,49 @@ import '../styles/CatalogoPelis.css';
 import { useEffect, useState } from 'react';
 import { getAllMovies } from '../services/movies';
 import { Spinner } from './Spinner';
-import { useLocation } from 'react-router-dom';
 import { useQuery } from  '../hooks/useQuery';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function CatalogoPelis (){
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [page, setPage] = useState(1);
 
     const query = useQuery();
     const search = query.get('search');
 
     useEffect(()=>{
         setIsLoading(true);
-        const searchUrl = search ? `/search/movie?query=${search}` : '/discover/movie';
+        const searchUrl = search 
+            ? `/search/movie?query=${search}&page=${page}` 
+            : `/discover/movie?page=${page}`;
         getAllMovies(searchUrl).then(data => {
-            setMovies(data.results);
+            //el preveMovies es una function creada para sólo esta 
+            //parte del codigo y añadir las peliculas a las ya cargadas
+            setMovies(prevMovies => prevMovies.concat(data.results));
             setIsLoading(false);
         });
-    },[search]);
+        //se establece de acuerdo el page al use state del mismo
+    },[search, page]);
     
 
-    if (isLoading){
-        return(
-            <Spinner/>
-        )
-    }
+
     return(
-        <ul className={'movieGrid'}>
-            {movies.map(movie =>(
-                <MovieCard key={movie.id}
-                movie={movie}/>
-            ))}
-        </ul>
+        //Esta librería requiere minimo 3 valores
+        <InfiniteScroll
+            dataLength={movies.length}
+            hasMore={true}
+            //se establece que se requiere con base en el useState
+            next={()=>setPage((prevePage) => prevePage + 1)}
+            loader ={<Spinner/>}
+        >
+            <ul className={'movieGrid'}>
+                {movies.map(movie =>(
+                    <MovieCard key={movie.id}
+                    movie={movie}/>
+                ))}
+            </ul>
+        </InfiniteScroll>
     )
 };
 
