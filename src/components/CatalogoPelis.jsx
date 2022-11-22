fccc678ec80fa4a7ba6b83dd70ac42003fe6eb29
Ivan Montenegro <nelson.moncar@outlook.com>
2022-11-22 11:32:49 -0500

    ultima modificación 22 de nov

--- src/components/CatalogoPelis.jsx
+++ src/components/CatalogoPelis.jsx
@@ -4,12 +4,15 @@ import { useEffect, useState } from 'react';
 import { getAllMovies } from '../services/movies';
 import { Spinner } from './Spinner';
 import { useQuery } from  '../hooks/useQuery';
+import { v4 as uuidv4 } from 'uuid';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 function CatalogoPelis (){
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [page, setPage] = useState(1);
+    //se usa este hook para que no cargue indefinidamente en busquedas
+    const [hasMore, setHasMore] = useState(true);
 
     const query = useQuery();
     const search = query.get('search');
@@ -23,6 +26,7 @@ function CatalogoPelis (){
             //el preveMovies es una function creada para sólo esta 
             //parte del codigo y añadir las peliculas a las ya cargadas
             setMovies(prevMovies => prevMovies.concat(data.results));
+            setHasMore(data.page < data.total_pages);
             setIsLoading(false);
         });
         //se establece de acuerdo el page al use state del mismo
@@ -34,14 +38,14 @@ function CatalogoPelis (){
         //Esta librería requiere minimo 3 valores
         <InfiniteScroll
             dataLength={movies.length}
-            hasMore={true}
+            hasMore={hasMore}
             //se establece que se requiere con base en el useState
             next={()=>setPage((prevePage) => prevePage + 1)}
             loader ={<Spinner/>}
         >
             <ul className={'movieGrid'}>
                 {movies.map(movie =>(
-                    <MovieCard key={movie.id}
+                    <MovieCard key={uuidv4()/* movie.id */}
                     movie={movie}/>
                 ))}
             </ul>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,17 @@
 import { CatalogoPelis } from '../components/CatalogoPelis';
 import { Searcher } from '../components/Searcher';
+import { useQuery } from '../hooks/useQuery';
 
 function LandingPage (){
+    //es un truco para resetear los hooks al establecer una key
+    //en el componente padre
+    const query = useQuery();
+    const search = query.get('search');
+
     return(
         <>
         <Searcher/>
-        <CatalogoPelis/>
+        <CatalogoPelis key={search}/>
         </>
     )
 };
